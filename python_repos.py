89cd7575ae164f7811d04835dbbf52e0762a709f
morifusa <morifusa@gmail.com>
2024-07-16 09:24:21 -0700

    modified:   python_repos.py

--- python_repos.py
+++ python_repos.py
@@ -13,3 +13,29 @@ response_dict = r.json()
 
 print("Total repositories:", response_dict['total_count'])
 
+# Explore information about repositories.
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+# Examine the first repository
+#repo_dicts = repo_dicts[1]
+
+print("\nSelected information about each repository")
+for repo_dict in repo_dicts:
+    print("Name:", repo_dict['name'])
+    print("Owner:", repo_dict['owner']['login'])
+    print("Stars:",repo_dict['stargazers_count'])
+    print("Repository URL:", repo_dict['html_url'])
+    print("Created", repo_dict['created_at'])
+    print("Updated:", repo_dict['updated_at'])
+    
+    description = repo_dict['description']
+    if description:  # Check if description is not None
+        print("Description:\n", description[:500] + "...")
+    else:
+        print("Description:\n None")
+    print("\n\n")
+
+# print("\nKeys:", len(repo_dicts))
+# for key in repo_dicts.keys():
+#    print(key)
