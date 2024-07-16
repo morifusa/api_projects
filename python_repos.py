81ee552da7281c8f8b743cb803ac42957701f255
morifusa <morifusa@gmail.com>
2024-07-15 17:12:30 -0700

    modified:   python_repos.py

--- python_repos.py
+++ python_repos.py
@@ -1,1 +1,6 @@
 import requests
+
+# Make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=languages:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
