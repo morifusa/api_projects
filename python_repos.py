4f7d1db10be9b9369149467a0101df7bce5b15c1
morifusa <morifusa@gmail.com>
2024-07-16 08:48:47 -0700

    modified:   python_repos.py

--- python_repos.py
+++ python_repos.py
@@ -4,3 +4,12 @@ import requests
 url = 'https://api.github.com/search/repositories?q=languages:python&sort=stars'
 r = requests.get(url)
 print("Status code:", r.status_code)
+
+# Store API response in a variable
+response_dict = r.json()
+
+# Process results.
+#print(response_dict.keys())
+
+print("Total repositories:", response_dict['total_count'])
+
